cd9ba889e651248afdd35b73e2822f62357f2772
Dave Pinch <davepinch@gmail.com>
2023-09-13 01:55:24 -0700

    Delete _site, overwrite .gitignore, update comment

--- migrate.ps1
+++ migrate.ps1
@@ -1,31 +1,36 @@
 
 #
-# Start of a script to migrate from Jekyll to Hugo
+# Script to migrate from Jekyll to Hugo
 #
 
 #
-# append new line to last line of .gitignore
-
-Add-Content -Path ".gitignore" -Value "`n.hugo_build.lock"
-
-
+# .gitignore (overwrite)
+#
+Set-Content -Path ".gitignore" -Value ".hugo_build.lock"
 
 #
-# Gemfile
+# _site (delete)
+#
+If (Test-Path -Path "_site") {
+  Remove-Item -Path "_site" -Recurse -Force
+}
+
+#
+# Gemfile (delete)
 # 
 If (Test-Path -Path "Gemfile") {
   Remove-Item -Path "Gemfile"
 }
 
 #
-# Gemfile.lock
+# Gemfile.lock (delete)
 #
 If (Test-Path -Path "Gemfile.lock") {
   Remove-Item -Path "Gemfile.lock"
 }
 
 #
-# go.mod
+# go.mod (create)
 #
 Set-Content -Path "go.mod" -Value @"
 module github.com/davepinch/pinchy.cc
@@ -33,7 +38,7 @@ go 1.21
 "@
 
 #
-# hugo.toml
+# hugo.toml (create)
 #
 Set-Content -Path "hugo.toml" -Value @"
 baseURL = 'https://pinchy.cc/'
