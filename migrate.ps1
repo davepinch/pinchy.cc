b3a8111c25764904395fcc8c9aee21ef65c1344b
Dave Pinch <davepinch@gmail.com>
2023-09-13 23:45:24 -0700

    Add viewport to baseof

--- migrate.ps1
+++ migrate.ps1
@@ -67,9 +67,10 @@ If (!(Test-Path -Path "layouts\_default")) {
 #
 Set-Content -Path "layouts\_default\baseof.html" -Value @"
 <!doctype html>
-<html>
+<html lang="en">
 <head>
  <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
  <title>{{ .Page.Title }}</title>
 </head>
 <body>
