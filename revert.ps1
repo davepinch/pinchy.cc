eed95f31d30eda0c540228d35779ffe661ffb94f
Dave Pinch <davepinch@gmail.com>
2023-09-30 01:21:45 -0700

    Remove check for hugo.yaml

--- revert.ps1
+++ revert.ps1
@@ -1,7 +1,5 @@
 # if hugo.yaml exists, delete .gitignore and then use git to revert all changes
 
-if (test-path -Path "hugo.yaml") {
-  Remove-Item -Path ".gitignore"
-  git reset --hard
-  git clean -fd
-}
+Remove-Item -Path ".gitignore"
+git reset --hard
+git clean -fd
