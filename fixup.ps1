82668642ea0ed38f3a45a27408cc03d9045ee540
Dave Pinch <davepinch@gmail.com>
2023-10-04 02:09:25 -0700

    Temporary script to move .md files to folders

---
+++ fixup.ps1
@@ -0,0 +1,13 @@
+(Get-ChildItem -Path "collections\_camera-roll" -Filter "*.md" -Recurse) | ForEach-Object {
+  
+    $file = $_
+    $name = $file.BaseName
+  
+    # Create a folder with the same name as the .md file (minus extension)
+    $oldDir = $file.DirectoryName
+    $newDir = Join-Path -Path $oldDir -ChildPath $name
+    New-Item -Path $newDir -ItemType Directory
+  
+    # Move the .md file into the new directory
+    Move-Item -Path $file.FullName -Destination $newDir
+}
