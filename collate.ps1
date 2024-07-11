b41968eec0047e1848d880e84bc8aa4c23cb2c68
Dave Pinch <davepinch@gmail.com>
2024-07-11 14:24:35 -0700

    Check if the file starts with ---

--- collate.ps1
+++ collate.ps1
@@ -6,12 +6,32 @@ $mdFiles = Get-ChildItem -Path $rootPath -Filter "*.md" -Recurse
 foreach ($mdFile in $mdFiles) {
 
     #
-    # A directory with a .md extension is not a markdown
-    # file and is most likely an error. Skip it.
+    # Check for a common error of a directory with a .md extension
     #
     if ($mdFile.Attributes -band [System.IO.FileAttributes]::Directory) {
         Write-Warning "Directory with .md extension: $($mdFile.FullName)"
         continue
     }
 
+    #
+    # Skip files called README.md
+    #
+    if ($mdFile.Name -eq "README.md") {
+        continue
+    }
+
+    #
+    # Get the contents of the file as a single string with the -Raw parameter.
+    # If -Raw is not used, Get-Content returns an array of strings.
+    #
+    $content = Get-Content -Path $mdFile.FullName -Raw
+
+    #
+    # Check if the first characters are ---
+    #
+    if ($content -notmatch '^---\r?\n') {
+        Write-Warning "Invalid file format: $($mdFile.FullName)"
+        continue
+    }
+    
 }
