4de79e7841810f977dbb6a9617726e1f06a0e5c6
davepinch <davepinch@gmail.com>
2024-11-12 14:26:24 -0800

    Show items in current folder

--- bbs.ps1
+++ bbs.ps1
@@ -1,1 +1,13 @@
+function Get-EmojiOpenFolder() {
+    return [System.Char]::ConvertFromUtf32(0x1F4C2)
+}
+
 Write-Host "hello world"
+
+Get-ChildItem | ForEach-Object {
+    if ($_.PSIsContainer) {
+        Write-Host "$(Get-EmojiOpenFolder) $_"
+    } else {
+        Write-Host "  $_"
+    }
+}
