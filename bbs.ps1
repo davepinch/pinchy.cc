e4361776302120c6e895d794cd803edd99a09c74
davepinch <davepinch@gmail.com>
2024-11-12 20:32:13 -0800

    Write emoji icon

--- bbs.ps1
+++ bbs.ps1
@@ -1,13 +1,33 @@
+function Get-EmojiFile() {
+    return [System.Char]::ConvertFromUtf32(0x1F4C4)
+}
+
 function Get-EmojiOpenFolder() {
     return [System.Char]::ConvertFromUtf32(0x1F4C2)
 }
 
-Write-Host "hello world"
-
-Get-ChildItem | ForEach-Object {
+function Get-Icon($item) {
     if ($_.PSIsContainer) {
-        Write-Host "$(Get-EmojiOpenFolder) $_"
+        return Get-EmojiOpenFolder
     } else {
-        Write-Host "  $_"
+        return Get-EmojiFile
     }
 }
+
+#
+# Functions beginning with an underscore (_) are prototypes that
+# are under development and will be renamed to conventional form.
+#
+
+function _DIR($criteria) {
+    Get-ChildItem | ForEach-Object {
+        if ($_.Name -like "$criteria*") {
+            Write-Host "$(Get-Icon($_)) $_"
+        }
+    }
+}
+
+Write-Host "hello world"
+_DIR
+$userInput = Read-Host ">>> "
+_DIR $userInput
